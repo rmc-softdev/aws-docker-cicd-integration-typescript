daea763adb687477048e65378bf4388f14c8541e
Rafael Marques <rafael.marques.cavalcante@gmail.com>
2021-01-26 11:03:31 -0300

    adding prettier and prettier jobs

--- src/App.js
+++ src/App.js
@@ -8,12 +8,7 @@ const App = () => {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           ITS WORKING!! HORAAAAY
         </a>
       </header>
