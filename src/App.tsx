0815cc4aafd8ffc7e8668c18775137370ecf4580
Rafael Cavalcante <rafael.marques.cavalcante@gmail.com>
2021-02-08 07:45:26 -0300

    introducing docker

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const App: React.FC = () => {
-  return <h1> if this works its a fucking big HORAAAAAAAAAY </h1>
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100vw', height: '100vh' }}>
+      <h1> hello world</h1>
+      <h5> This is, without a doubt, the hardest hello world you will find out there.</h5>
+    </div>
+  )
 }
 
 export default App
